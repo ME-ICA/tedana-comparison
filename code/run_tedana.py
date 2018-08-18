1625d77633633739f18810484984d17198845c35
Taylor Salo <tsalo006@fiu.edu>
2018-08-18 19:21:18 -0400

    Add script to run the ME-ICA/tedana pipeline
    The only problem is that the arguments “out_dir’ and
    ”component_algorithm” aren’t implemented yet.

---
+++ code/run_tedana.py
@@ -0,0 +1,33 @@
+"""
+Run meica.py
+"""
+import json
+# import subprocess
+import os.path as op
+
+from tedana.workflows import tedana_workflow
+
+from utils import get_preproc_data
+
+
+if __name__ == '__main__':
+    with open('dset_config.json', 'r') as fo:
+        CONFIG = json.load(fo)
+
+    DATA_DIR = op.abspath('../data')
+
+    for dset_name in CONFIG.keys():
+        for item_cfg in CONFIG[dset_name]:
+            files, tes = get_preproc_data(dset_name, item_cfg, DATA_DIR)
+            ds_dir = op.join(DATA_DIR, dset_name, item_cfg['version'],
+                             'uncompressed')
+
+            # Run v2.5 of the component selection algorithm
+            out_dir = op.join(ds_dir, 'derivatives/tedana_v2.5/')
+            tedana_workflow(data=files, tes=tes, out_dir=out_dir,
+                            component_algorithm='v2.5')
+
+            # Run v3.2 of the component selection algorithm
+            out_dir = op.join(ds_dir, 'derivatives/tedana_v3.2/')
+            tedana_workflow(data=files, tes=tes, out_dir=out_dir,
+                            component_algorithm='v3.2')
