068d29d464fbfdee449ee280f40faac7627c7e3c
Taylor Salo <tsalo006@fiu.edu>
2018-09-01 17:40:15 -0400

    Simplify output directory structure.

--- code/download_data.py
+++ code/download_data.py
@@ -5,6 +5,7 @@
 Download datasets for comparison from OpenNeuro.
 """
 import json
+import shutil
 import os.path as op
 
 from nistats.datasets import (fetch_openneuro_dataset_index,
@@ -21,12 +22,14 @@ def download_dataset(cfg):
 
     # Just download based on subject for now.
     # Don't want to accidentally ignore anats or field maps.
-    filters = ['*sub-{0}*'.format(cfg['subject'])]
-    urls = select_from_index(urls, inclusion_filters=filters)
-    temp_urls1 = [url for url in urls if 'derivatives' not in url]
-    temp_urls2 = [url for url in urls if 'derivatives/fmriprep' in url]
-    urls = temp_urls1 + temp_urls2
-    print('\n\t'.join(urls))
+    sub = 'sub-{0}'.format(cfg['subject'])
+    urls = select_from_index(urls)
+    temp_urls1 = [url for url in urls if ('derivatives' not in url) and
+                  (sub in url)]
+    temp_urls2 = [url for url in urls if ('derivatives' not in url) and
+                  ('sub-' not in url)]
+    urls = sorted(list(set(temp_urls1 + temp_urls2)))
+
     _, _ = fetch_openneuro_dataset(urls=urls, dataset_version=dataset_version,
                                    data_dir=op.abspath('../data/'))
 
@@ -38,3 +41,13 @@ if __name__ == '__main__':
     for dset in CONFIG.keys():
         for item_cfg in CONFIG[dset]:
             download_dataset(item_cfg)
+
+        dataset_version = item_cfg['version']
+        dataset = dataset_version.split('_')[0]
+        dset_dir = op.abspath('../data/{0}/{1}/uncompressed'
+                              '/'.format(dataset, dataset_version))
+        temp = op.abspath('../data/temp/')
+        dest = op.abspath('../data/{0}/'.format(dataset))
+        shutil.move(dset_dir, temp)
+        shutil.rmtree(dest)
+        shutil.move(temp, dest)
