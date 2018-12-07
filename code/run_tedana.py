09cea1f5e317bef509a8dd9775efaa19ddd1a663
Taylor Salo <tsalo006@fiu.edu>
2018-12-06 20:03:21 -0500

    Add functions to run linearly. Should work.

--- code/run_tedana.py
+++ code/run_tedana.py
@@ -14,33 +14,81 @@ tedort: on and off
 """
 import re
 import json
-# import subprocess
 import os.path as op
 
 from tedana.workflows import tedana_workflow
 
-from utils import get_preproc_data
+
+def make_str(dict_):
+    """
+    Make name string from dictionary of values.
+    """
+    ste_name_dict = {0: 'catd', -1: 'optcom'}
+    lst = []
+    for key, value in dict_.items():
+        if key == 'ste':
+            # we don't want that -1 because of the hyphen
+            value = ste_name_dict[value]
+
+        # Join list arguments (i.e., gscontrol) with ampersand
+        if isinstance(value, list):
+            value = '&'.join(value)
+
+        lst.append('{0}-{1}'.format(str(key), str(value)))
+    str_ = '_'.join(lst)
+    return str_
 
 
 def run_tedana(files, tes, seed):
+    """
+    Run tedana workflow across a range of parameters
+
+    Parameters
+    ----------
+    files : list of str
+        Echo-specific preprocessed data files
+    tes : list of floats
+        Echo times in seconds
+    seed : int
+        Random seed
+    """
     tes = [te * 1000 for te in tes]
     sub = re.findall('sub-[0-9a-zA-Z]+_', files[0])[0][:-1]
     ds_dir = files[0][:files[0].index(sub)]
 
-    # Baseline
-    # tedpca=mle, gscontrol=None, combmode=t2s, sourceTEs=-1, no wvpca, no tedort
-    name = ('tedana_tedpca-mle_gscontrol-None_combmode-t2s_ste-optcom_'
-            'wvpca-False_tedort-False_seed-{0:03d}'.format(seed))
-    out_dir = op.join(ds_dir, 'derivatives', name, sub, 'func')
-    tedana_workflow(data=files, tes=tes, tedpca='mle', gscontrol=None,
-                    combmode='t2s', ste=-1, wvpca=False, tedort=False,
-                    out_dir=out_dir)
+    for combmode in ['t2s', 'linear']:
+        for gscontrol in [['t1c'], ['gsr', 't1c']]:
+            for ste in [-1, 0]:
+                for tedort in [False, True]:
+                    for tedpca in ['mle', 'kundu']:
+                        for wvpca in [False, True]:
+                            args = {'combmode': combmode,
+                                    'gscontrol': gscontrol,
+                                    'ste': ste,
+                                    'tedort': tedort,
+                                    'tedpca': tedpca,
+                                    'wvpca': wvpca}
+                            name = make_str(args)
+                            name = name + '_seed-{0:03d}'.format(seed)
+                            out_dir = op.join(ds_dir, 'derivatives', name, sub, 'func')
+                            tedana_workflow(data=files, tes=tes, fixed_seed=seed,
+                                            out_dir=out_dir, debug=True, **args)
 
-    # Fit models to concatenated data (sourceTEs = 0)
-    # tedpca=mle, gscontrol=None, combmode=t2s, sourceTEs=0, no wvpca, no tedort
-    name = ('tedana_tedpca-mle_gscontrol-None_combmode-t2s_ste-all_'
-            'wvpca-False_tedort-False_seed-{0:03d}'.format(seed))
-    out_dir = op.join(ds_dir, 'derivatives', name, sub, 'func')
-    tedana_workflow(data=files, tes=tes, tedpca='mle', gscontrol=None,
-                    combmode='t2s', ste=0, wvpca=False, tedort=False,
-                    out_dir=out_dir)
+
+def run_dset(dset_dict):
+    n_iterations = 1
+    for sub in dset_dict.keys():
+        for run in dset_dict[sub].keys():
+            files = dset_dict[sub][run]['files']
+            tes = dset_dict[sub][run]['echo_times']
+            for seed in range(n_iterations):
+                run_tedana(files, tes, seed)
+
+
+if __name__ == '__main__':
+    with open('all_files.json', 'r') as fo:
+        all_dict = json.load(fo)
+
+    for dset in all_dict.keys():
+        dataset_dict = all_dict[dset]
+        run_dset(dataset_dict)
